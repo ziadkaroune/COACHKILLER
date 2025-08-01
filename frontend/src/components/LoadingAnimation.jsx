efc23034215761e9f028d67fa18896ee74d951db
zivd <ziad99kr@gmail.com>
2025-08-01 15:23:06 +0200

    new loading animation

---
+++ frontend/src/components/LoadingAnimation.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Riple from "react-loading-indicators/OrbitProgress";
+
+const LoadingA = () => (
+	<Riple color="black" size="medium" text="" textColor="black" />
+);
+
+export default LoadingA;
